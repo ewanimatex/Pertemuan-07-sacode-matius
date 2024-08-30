293c07269ebe274562631230ee079d789ab9f01c
matius ewani <ewanimatex@gmail.com>
2024-08-29 19:29:48 -0700

    komit pertama

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// ingle line comment
+
+
+/*
+
+multiple line comment
+bisa tulus juga di sini
+*/
+
+var namaDepan =  "Matius"
+var namaBelakang ="Ewani"
+
+document.getElementById("nama")
+.linearHTML = namaDepan + namaBelakang;
